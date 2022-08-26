65a0c5da7e8f27d17e05b76fb9092b9039e3d91c
Vedansh <96698078+vedu-alive@users.noreply.github.com>
2022-08-26 09:29:51 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,7 @@
+import Image from "next/image";
 function Avatar({ url, className }) {
   return (
-    <img
+    <Image
       className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       loading="lazy"
       src={url}
