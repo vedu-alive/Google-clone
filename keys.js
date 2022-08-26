59313880707778ec612a7f3c783f196a7b7db7d4
Vedansh <96698078+vedu-alive@users.noreply.github.com>
2022-08-26 09:34:20 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 //API Key - https://developers.google.com/custom-search/v1/using_rest
 
-export const API_KEY = "### Your key ###";
+export const API_KEY = "AIzaSyC38J1bWlHycY1kaoi6_kP6alR9OtjOGrE";
 
 //Context key- https://cse.google.com/cse/create/new
 
